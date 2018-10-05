49cfddb321dac2c368f2653943ad57a0e6c77fb6
bogdancrisan <43874895+bogdancrisan@users.noreply.github.com>
2018-10-05 10:41:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+const genId = () => Math.random().toString().slice(2);
+
+const randomSizeOf = num => Math.floor(Math.random() * num);
+
+const COLORS = ['white', 'black', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const randomColor = () => COLORS[randomSizeOf(COLORS.length)];
+
+const randomOfWidth = view => randomSizeOf(view.getCanvasWidth());
+
+const randomOfHeight = view => randomSizeOf(view.getCanvasHeight());
+
+const testImage = args => view.Image({...args, url: 'hero@2x.jpg', top: randomOfHeight(view), left: randomOfWidth(view)});
+
+const testText = view => view.Text(genId(), {fill: randomColor(), top: randomOfHeight(view), left: randomOfWidth(view)});
+
+const testRect = view => view.Rect({
+  fill: randomColor(),
+  top: randomOfHeight(view),
+  left: randomOfWidth(view),
+  height: randomOfHeight(view),
+  width: randomOfWidth(view),
+});
+
+const generateElements = (num, fn) => {
+  const texts = Array(num);
+  for (let i = 0; i < num; i++) {
+    texts[i] = fn();
+  }
+  return texts;
+}
+
+function createLink(src, name) {
+  const a = document.createElement('a');
+  
+  a.href = src;
+  a.textContent = name;
+  a.download = name;
+  
+  document.body.appendChild(a);
+}
+
+
+window.onload = function() {
+  const view = Object.create(View);
+
+  const canvas = document.querySelector('#canvas');
+
+  canvas.setAttribute('width', 12000);
+  canvas.setAttribute('height', 12000);
+
+  view.init('canvas');
+
+  Promise.all(generateElements(4, testImage.bind(null, view)))
+  .then((images) => {
+    view.addElements(generateElements(100, testRect.bind(null, view)));
+    view.addElements(generateElements(100, testText.bind(null, view)));
+    view.addElements(images);
+    view.render();
+    console.log(view.toJSON());
+  })
+  
+}
---
+++ utils.js
@@ -0,0 +1,92 @@
+
+const U = {};
+
+/**
+ * Returns a new object with the desired properties from the original object
+ * @param  {...any} props 
+ */
+U.select = (...props) => obj => props.reduce((acc, prop) => ({ ...acc, [prop]: obj[prop]}), {});
+
+/**
+ * @param  {...function} args - functions that operate in order from right
+ *  to left on the supplied value
+ */
+U.compose = (...args) => val => args.reduceRight((acc, fn) => fn(acc), val);
+
+U.log = obj => (console.log(obj), obj);
+
+U.get = prop => obj => obj[prop];
+
+U.mmsToPixels = mms => Math.floor(mms * 3.7795275591);
+
+/**
+ * get object values based on separate search terms to try and not depend on the specific object key name
+ * @param  {...string} terms 
+ */
+U.getProperty = (...terms) => obj => {
+  const prop = Object.keys(obj).find(key => terms.filter(term => key.toLowerCase().includes(term)).length === terms.length);
+  
+  if (!prop) {
+    return undefined;
+  }
+  
+  return obj[prop];
+}
+
+U.getPropertyInArray = (...terms) => arr => U.getProperty(...terms)(arr.find(element => U.getProperty(...terms)(element)));
+
+U.isNumber = str => !Number.isNaN(parseInt(str));
+
+U.getPercent = (percent, num) => Math.floor((percent / 100) * num);
+
+/**
+ * @param  {string} terms - check to see if these are included in the supplied string
+ * @param {string} str - string
+ */
+U.contains = (...terms) => str => terms.filter(term => str.toLowerCase().includes(term)).length === terms.length;
+
+/**
+ * Returns an array with only the objects that have these matching key value pairs
+ * @param {string} key 
+ * @param {any} value 
+ * @param {obj[]} arr 
+ */
+U.selectWith = (key, value) => arr => arr.filter(el => U.getProperty(key)(el) === value);
+
+U.selectWithout = (key, value) => arr => arr.filter(el => U.getProperty(key)(el) !== value);
+
+/**
+ * Replace objects in existingArr that have matching key value pairs with newElements
+ * @param {string} key 
+ * @param {any} value 
+ * @param {obj[]} existingArr 
+ * @param {obj[]} newElements 
+ */
+U.replace = (key, value, existingArr, newElements) => [ ...U.selectWithout(key, value)(existingArr), ...newElements];
+
+U.not = obj => !obj;
+
+U.isPromise = obj => !!obj.then;
+
+/**
+ * Returns a function that can be passed as a callback and it will call fn with val as parameter
+ * @param {function} fn 
+ * @param {any} val 
+ */
+U.callback = (fn, val) => () => fn(val);
+
+/**
+ * Generates a class string based on the base class name and the modifiers provided
+ * @param {string} cssclass 
+ * @param {string} modifiers 
+ */
+U.genCssClasses = (cssclass, modifiers) => `${cssclass} ${modifiers.split(/\s+/).map(mod => `${cssclass}--${mod}`).join(' ')}`;
+
+U.getBrowserWidth = () => (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+
+/**
+ * Random 16 characters long number id
+ */
+U.genId = () => Math.random().toString().slice(2);
+
+// export default U;
